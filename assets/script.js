9fede09df5d71590bc05ea6e43fbb1224403a2d6
Victoria G <125226074+victoria-cg@users.noreply.github.com>
2024-02-05 21:14:56 -0600

    removed duplicate repo, commented out broken function

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,28 @@
 //function to get data for SerpAPI Google Flights API. Currently using JSON placeholder API -VG
-function getApi() {
-    //sets variable to get the URL for the API -VG
-    var requestFlightUrl = 'https://jsonplaceholder.typicode.com/todos/1';
-    // fetches the variable for the API URL, stores its response in JSON? -VG
-    fetch(requestFlightUrl)
-    .then(function (response) {
-      return response.json();
-    })
+// async function getApi() {
+//   var flightApiKey = '&api_key=c404903e1b82f634995e519657fdfc7cf3033f11612475935d15434eea93e161';
+//     //sets variable to get the URL for the API -VG
+//     var requestFlightUrl = 'https://serpapi.com/search.json?engine=google_flights&departure_id=PEK&arrival_id=AUS&outbound_date=2024-02-06&return_date=2024-02-12&currency=USD&hl=en' + flightApiKey;
+//     // fetches the variable for the API URL -VG
+//     var res = await fetch(requestFlightUrl, {
+//       method: 'GET', 
+//       mode: 'no-cors',
+//       headers: {
+//         'Content-Type': 'application/json',
+        
+//       },
+//     })
+//     var data = await res.json();
+//     console.log(data);
+//     // //.then(res => res.json())
+    // .then(data => console.log(data))
+    // .catch(error=>console.log(error));//
 
-    //Then we need fucntions here to get the data from the API and append it to HTML elements so that it displays on the screen, see class activity 6.3 -VG
-}
-//calls the getApi function
-getApi();
-console.log(getApi);
-
-//These are variables so we can change the city the user looks for later in the project
+//     }
+//     //Then we need fucntions here to get the data from the API and append it to HTML elements so that it displays on the screen, see class activity 6.3 
+// //calls the getApi function
+// getApi();
+// //These are variables so we can change the city the user looks for later in the project
 let apiKey = '826bbb162ffd06dab609a97389a04db7';
 let city = 'Houston';
 
